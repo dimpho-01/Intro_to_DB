9513df3abee33ca2dd6b2f0ac4758fd8b28a1b9b
dimpho-01 <dimpho.matlou@gmail.com>
2024-07-28 05:04:08 +0200

    Modified file task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,5 @@
 USE alx_book_store;
-SELECT * from COLUMNS WHERE TABLE_SCHEMA = 'alx_book_store' and TABLE_NAME = 'books';
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'Books';
